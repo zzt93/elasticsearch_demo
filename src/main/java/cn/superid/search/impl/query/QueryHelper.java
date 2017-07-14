3275d0339d4158ca0f48927f809f2c68836cc71d
zzt <zengzt93@gmail.com>
2017-07-14 09:26:06 +0800

    add announcement highlight imple;

---
+++ src/main/java/cn/superid/search/impl/query/QueryHelper.java
@@ -0,0 +1,24 @@
+package cn.superid.search.impl.query;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author zzt
+ */
+public class QueryHelper {
+
+
+    private static final Pattern PARAMETER_PLACEHOLDER = Pattern.compile("\\?(\\d+)");
+
+    public static String replacePlaceholders(String input, String... var) {
+        Matcher matcher = PARAMETER_PLACEHOLDER.matcher(input);
+        String result = input;
+        while (matcher.find()) {
+            String group = matcher.group();
+            int index = Integer.parseInt(matcher.group(1));
+            result = result.replace(group, var[index]);
+        }
+        return result;
+    }
+}
--- src/main/java/cn/superid/search/impl/query/time/announcement/AnnouncementRepoImpl.java
+++ src/main/java/cn/superid/search/impl/query/time/announcement/AnnouncementRepoImpl.java
@@ -1,6 +1,7 @@
 package cn.superid.search.impl.query.time.announcement;
 
 import cn.superid.search.entities.time.Announcement;
+import cn.superid.search.impl.query.QueryHelper;
 import org.elasticsearch.search.highlight.HighlightBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -9,39 +10,24 @@ import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.data.elasticsearch.core.query.SearchQuery;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import static org.elasticsearch.index.query.QueryBuilders.wrapperQuery;
 
 /**
  * @author zzt
  */
 public class AnnouncementRepoImpl implements AnnouncementCustom {
-    private static final Pattern PARAMETER_PLACEHOLDER = Pattern.compile("\\?(\\d+)");
 
     @Autowired
     private ElasticsearchTemplate template;
 
     @Override
     public Page<Announcement> findByTitleOrModifierRoleOrModifierUserOrTagsIn(String info, Pageable pageable) {
-        String query = replacePlaceholders(FIND_BY_TITLE_OR_CONTENT_OR_MODIFIER_ROLE_OR_MODIFIER_USER_OR_TAGS_IN_QUERY, info);
+        String query = QueryHelper.replacePlaceholders(FIND_BY_TITLE_OR_CONTENT_OR_MODIFIER_ROLE_OR_MODIFIER_USER_OR_TAGS_IN_QUERY, info);
         SearchQuery searchQuery = new NativeSearchQueryBuilder()
                 .withQuery(wrapperQuery(query))
                 .withHighlightFields(new HighlightBuilder.Field("title"), new HighlightBuilder.Field("content"))
                 .build();
         return template.queryForPage(searchQuery, Announcement.class);
-    }
-
-    private String replacePlaceholders(String input, String... var) {
-        Matcher matcher = PARAMETER_PLACEHOLDER.matcher(input);
-        String result = input;
-        while (matcher.find()) {
-            String group = matcher.group();
-            int index = Integer.parseInt(matcher.group(1));
-            result = result.replace(group, var[index]);
-        }
-        return result;
     }
 
 }
