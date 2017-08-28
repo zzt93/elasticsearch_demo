e1d8f5d12951cfd1973ad52fd379c3257a997777
zzt <zengzt93@gmail.com>
2017-08-28 16:19:47 +0800

    make remote debugable;

--- searcher.sh
+++ searcher.sh
@@ -1,3 +1,3 @@
 export BUILD_ID=dontKillMe
 fuser -k -n tcp 9500
-nohup java -jar ./searchlogic/target/search-logic-0.0.1-SNAPSHOT.jar &
+nohup java "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005" -jar ./searchlogic/target/search-logic-0.0.1-SNAPSHOT.jar &
