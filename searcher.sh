0e05c3c3a49dc86ca041245f0cf265d97ae82158
ypc <ypc@superid.cn>
2017-09-14 16:00:41 +0800

    pom文件整理

--- searcher.sh
+++ searcher.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 export BUILD_ID=dontKillMe
 fuser -k -n tcp 9500
 nohup java "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005" -jar ./searchlogic/target/search-logic-0.0.1-SNAPSHOT.jar &
