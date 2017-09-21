18022acae8fab3b8187c0c7dc968f0aadc202a03
zzt <zengzt93@gmail.com>
2017-09-21 10:23:31 +0800

    add environment setting in script.

--- searcher.sh
+++ searcher.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 export BUILD_ID=dontKillMe
 fuser -k -n tcp 9500
+ES_USER=$1
 nohup java "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005" -jar ./searchlogic/target/search-logic-0.0.1-SNAPSHOT.jar &
