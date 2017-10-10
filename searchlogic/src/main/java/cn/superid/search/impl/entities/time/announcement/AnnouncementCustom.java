2e33568bca12a9c41b1d3e19cd5427aa7179dbc6
zzt <zengzt93@gmail.com>
2017-10-10 10:43:23 +0800

    fix the bug of must and should.

--- searchlogic/src/main/java/cn/superid/search/impl/entities/time/announcement/AnnouncementCustom.java
+++ searchlogic/src/main/java/cn/superid/search/impl/entities/time/announcement/AnnouncementCustom.java
@@ -10,40 +10,52 @@ import org.springframework.data.domain.Pageable;
 public interface AnnouncementCustom {
 
   String FIND_BY_TITLE_OR_CONTENT_OR_MODIFIER_ROLE_OR_MODIFIER_USER_OR_TAGS_IN_QUERY =
-      " {" +
-          " \"bool\": {\n" +
-          "     \"filter\": {\n" +
-          "          \"terms\" : { " +
-          "             \"affairId\" : ?1" +
-          "           }" +
-          "       },\n" +
-          "     \"should\": [\n" +
-          "       {\n" +
-          "         \"multi_match\": {\n" +
-          "           \"query\": \"?0\",\n" +
-          "           \"fields\": [\"title\", \"content\", \"creatorRole\", \"creatorUser\", \"affairName\"]\n" +
-          "         }\n" +
-          "       },\n" +
-          "       {\n" +
-          "         \"nested\": {\n" +
-          "           \"path\": \"tags\",\n" +
-          "           \"query\": {\n" +
-          "             \"match\": {\n" +
-          "               \"tags.des\": \"?0\"\n" +
-          "             }}\n" +
-          "         }\n" +
-          "       } " +
-          "      ],\n" + "      \"must\": {\n" +
-          "        \"range\" : {\n" +
-          "          \"modifyTime\":{\n" +
-          "            \"gt\": \"?2\",\n" +
-          "            \"lte\": \"?3\"\n" + ",\n" +
-          "                \"format\": \"yyyy-MM-dd hh:mm:ss\"" +
-          "          }\n" +
-          "        }\n" +
-          "      }" +
-          "  }" +
-          "}";
+      "{\n"
+          + "  \"bool\": {\n"
+          + "    \"filter\": {\n"
+          + "      \"terms\": {\n"
+          + "        \"affairId\": ?1"
+          + "      }\n"
+          + "    }, \n"
+          + "    \"must\": [\n"
+          + "      {\n"
+          + "        \"range\": {\n"
+          + "          \"modifyTime\": {\n"
+          + "            \"gt\": \"?2\", \n"
+          + "            \"lte\": \"?3\", \n"
+          + "            \"format\": \"yyyy-MM-ddhh:mm:ss\"\n"
+          + "          }\n"
+          + "        }\n"
+          + "      }, \n"
+          + "      {\n"
+          + "        \"should\": [\n"
+          + "          {\n"
+          + "            \"multi_match\": {\n"
+          + "              \"query\": \"?0\", \n"
+          + "              \"fields\": [\n"
+          + "                \"title\", \n"
+          + "                \"content\", \n"
+          + "                \"creatorRole\", \n"
+          + "                \"creatorUser\", \n"
+          + "                \"affairName\"\n"
+          + "              ]\n"
+          + "            }\n"
+          + "          }, \n"
+          + "          {\n"
+          + "            \"nested\": {\n"
+          + "              \"path\": \"tags\", \n"
+          + "              \"query\": {\n"
+          + "                \"match\": {\n"
+          + "                  \"tags.des\": \"?0\"\n"
+          + "                }\n"
+          + "              }\n"
+          + "            }\n"
+          + "          }\n"
+          + "        ]\n"
+          + "      }\n"
+          + "    ]\n"
+          + "  }\n"
+          + "}";
 
   Page<AnnouncementPO> findByTitleOrContentOrCreatorRoleOrCreatorUserOrAffairNameOrTagsInAffair(
       List<Long> affairIds, String info,
