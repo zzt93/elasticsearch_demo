098ee1e41406177157e6f1428d7c2d1bf3f78d06
zzt <zengzt93@gmail.com>
2018-08-22 18:00:06 +0800

    add target related po/vo.

--- searchentity/src/main/java/cn/superid/search/entities/user/task/TaskQuery.java
+++ searchentity/src/main/java/cn/superid/search/entities/user/task/TaskQuery.java
@@ -8,8 +8,35 @@ import java.util.List;
  */
 public class TaskQuery extends PagedQuery {
 
+  private Long affairId;
+  private Long targetId;
+  private List<Byte> types;
   private Byte state;
   private List<Long> roles;
+
+  public List<Byte> getTypes() {
+    return types;
+  }
+
+  public void setTypes(List<Byte> types) {
+    this.types = types;
+  }
+
+  public Long getAffairId() {
+    return affairId;
+  }
+
+  public void setAffairId(Long affairId) {
+    this.affairId = affairId;
+  }
+
+  public Long getTargetId() {
+    return targetId;
+  }
+
+  public void setTargetId(Long targetId) {
+    this.targetId = targetId;
+  }
 
   public List<Long> getRoles() {
     return roles;
