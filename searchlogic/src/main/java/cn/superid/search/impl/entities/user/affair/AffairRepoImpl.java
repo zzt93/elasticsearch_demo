755017b3da26cf574ac720df1082a4a4f6c37f64
zzt <zengzt93@gmail.com>
2019-08-13 10:52:01 +0800

    make only search public affair.

--- searchlogic/src/main/java/cn/superid/search/impl/entities/user/affair/AffairRepoImpl.java
+++ searchlogic/src/main/java/cn/superid/search/impl/entities/user/affair/AffairRepoImpl.java
@@ -5,6 +5,7 @@ import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
 import static org.elasticsearch.index.query.QueryBuilders.termQuery;
 import static org.elasticsearch.index.query.QueryBuilders.wildcardQuery;
 
+import cn.superid.common.rest.type.PublicType;
 import cn.superid.search.impl.save.rolling.Suffix;
 import java.util.List;
 import org.elasticsearch.index.query.BoolQueryBuilder;
@@ -27,7 +28,7 @@ public class AffairRepoImpl implements AffairCustom {
   public Page<AffairPO> findAny(String info, Pageable pageable) {
     BoolQueryBuilder bool = boolQuery()
         .must(termQuery("state", 0))
-//        .must(termQuery("publicType", PublicType.ALL))
+        .must(termQuery("publicType", PublicType.ALL))
         .must(
             boolQuery()
                 .should(termQuery("superId", info))
@@ -46,7 +47,7 @@ public class AffairRepoImpl implements AffairCustom {
   public List<AffairPO> findAlliance(String info, Long allianceId, Pageable pageable) {
     BoolQueryBuilder bool = boolQuery()
         .must(termQuery("state", 0))
-//        .must(termQuery("publicType", PublicType.ALL))
+        .must(termQuery("publicType", PublicType.ALL))
         .must(termQuery("parentId", 0))
         .must(
             boolQuery()
