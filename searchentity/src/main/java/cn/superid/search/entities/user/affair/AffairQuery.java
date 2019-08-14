433c7f9fa5f53f719a4f38dfd056a5bf5dbb8d0b
zzt <zengzt93@gmail.com>
2019-08-14 15:28:25 +0800

    add molds for search.

--- searchentity/src/main/java/cn/superid/search/entities/user/affair/AffairQuery.java
+++ searchentity/src/main/java/cn/superid/search/entities/user/affair/AffairQuery.java
@@ -1,6 +1,7 @@
 package cn.superid.search.entities.user.affair;
 
 import cn.superid.search.entities.PagedQuery;
+import java.util.List;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -16,7 +17,7 @@ import org.springframework.data.domain.PageRequest;
 public class AffairQuery extends PagedQuery {
   private String[] tags;
   private long allianceId;
-  private Byte mold;
+  private List<Byte> molds;
 
   public AffairQuery(String query, PageRequest pageRequest) {
     setQuery(query);
@@ -28,7 +29,6 @@ public class AffairQuery extends PagedQuery {
     setPageRequest(pageRequest);
     this.tags = tags;
   }
-
 
   public AffairQuery setAllianceId(long allianceId) {
     this.allianceId = allianceId;
